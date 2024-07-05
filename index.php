b0b32ea1f242f4290f843cb3b298c463cc38d1b0
maximekgn <kogonmaxime@gmail.com>
2024-07-05 15:59:07 +0000

    #

--- index.php
+++ index.php
@@ -8,50 +8,55 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <title>register</title>
+    <title>Inscription</title>
 </head>
-<body>
-    <form action="<?php htmlspecialchars($_SESSION['PHP_SELF']) ?>" method="post" class="flex flex-col">
+<body class="flex justify-center items-center h-screen flex-col">
 
-        <h2>Bienvenue Chez MXMKGN</h2>
-        <div>
-            <label for="username">Username</label>
-            <input type="text" name="username" id="username" required>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" class="border border-gray-300 rounded-lg p-8 shadow-xl block ">
+
+        <h2 class="text-4xl font-bold mb-6 text-center">Inscription</h2>
+        <div class="mb-4">
+            <label for="username" class="block text-gray-700 text-lg font-bold mb-2">Nom d'utilisateur</label>
+            <input type="text" name="username" id="username" placeholder="nom d'utilisateur" class="border border-gray-300 rounded-lg px-4 py-2 w-full">
         </div>
-        <div>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" required>
+        <div class="mb-6">
+            <label for="password" class="text-gray-700 text-lg font-bold mb-2">Mot de Passe</label>
+            <input type="password" name="password" id="password" placeholder="mot de passe" class="border border-gray-300 rounded-lg px-4 py-2 w-full">
         </div>
-        <div>
-            <input type="submit" value="register" name="register">
+        <div class="flex items-center justify-center">
+            <button type="submit" name="register" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline " id="btn">S'inscrire</button>
         </div>
-        
     </form>
 </body>
+
 </html>
 
 <?php 
 
     if (isset($_POST["register"])){
+        $username = filter_input(INPUT_POST , "username" , FILTER_SANITIZE_SPECIAL_CHARS) ;
+        $password = filter_input(INPUT_POST , "password" , FILTER_SANITIZE_SPECIAL_CHARS) ;
 
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $username = filter_input(INPUT_POST , "username" , FILTER_SANITIZE_SPECIAL_CHARS) ;
-            $password = filter_input(INPUT_POST , "password" , FILTER_SANITIZE_SPECIAL_CHARS) ;
+        $username = trim($username) ; 
+        $password = trim($password) ;
 
-            if (empty($username)) echo "Please enter a username ";
-            elseif (empty($password)) echo "Please enter a Password" ;
+        try {
+            if (empty($username) || empty($password) || ctype_space($username) || ctype_space($password)) echo "<p class='mt-12 text-red-500 text-xl font-bold'>Champs vides ou juste composé d'espaces</p>" ;
             else {
                 $hash = password_hash($password , PASSWORD_DEFAULT) ;
                 $sql = "INSERT INTO users (user,password)
                         VALUES 
-                        ('$username' , '$hash')" ;
+                        ('$username' , '$hash') ;" ; //the sql query that will be executed on the database
                 mysqli_query($conn , $sql) ;
 
-                echo "You are now registred !" ;
+                echo "<p class='mt-12 text-green-500 text-3xl font-bold'>Inscrition réussie</p>" ;
             }
-
         }
-    }
+        //check if there is another user with the same name
+        catch (mysqli_sql_exception){
+            echo "<p class='mt-12 text-red-500 text-xl font-bold'>Nom d'utilisateur deja pris</p>" ;
+        }
 
-    mysqli_close($conn) ;
+    mysqli_close($conn) ; 
+    }
 ?>
